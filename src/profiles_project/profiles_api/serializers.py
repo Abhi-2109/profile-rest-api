b6767fc55d85beb0ea0ae2a8c1fdf8d1d549e86e
Abhishek Anand <abhishek197770@gmail.com>
2019-01-24 03:52:36 +0530

    Added First Serializer and Post Methods

---
+++ src/profiles_project/profiles_api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+
+class HelloSerializer(serializers.Serializer):
+    """
+        Serializes a name field for testing our APIViews
+    """
+    name = serializers.CharField(max_length = 10)
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 # Create your views here.
 class HelloApiView(APIView):
     """
     Test API View
     """
+    serializer_class = serializers.HelloSerializer
+
     def get(self,request,format = None):
         """Return a list of APIView features"""
 
@@ -19,3 +24,14 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message' : 'Hello!','an_apiview':an_apiview})
+
+    def post(self, request):
+        """ Create a Hello Message with our name """
+
+        serializer = serializers.HelloSerializer(data = request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        return Response(
+            serializer.errors, status = status.HTTP_400_BAD_REQUEST)
